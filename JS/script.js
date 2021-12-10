9cc888d1414a8d8ea3ce37f23857168d8508ff5b
wesleychuu <wesleychu@telus.net>
2021-12-10 13:20:20 -0800

    added contactform

--- JS/script.js
+++ JS/script.js
@@ -3,4 +3,20 @@ let togglebutton = document.getElementsByClassName("toggle")[0];
 togglebutton.addEventListener('click', function(event) {
     let body = document.querySelector('body');
     body.classList.toggle('dark-theme');
-})
+});
+
+
+
+let submitbutton = document.querySelector('#frm-btn');
+
+submitbutton.addEventListener('click', function(event) {
+    event.preventDefault();
+    let form = document.querySelector('.myForm');
+    myinfo = {
+        name: form.elements[1].value,
+        email: form.elements[2].value,
+        message: form.elements[0].value
+    };
+    console.log(myinfo);
+});
+
